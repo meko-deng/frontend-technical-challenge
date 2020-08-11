e7724a60ca9b63c68758c7b2b64bd3790504b4aa
Mekodeng94 <mekodeng94@gmail.com>
2020-08-11 11:31:56 -0400

    Added review stars component

--- components/review-stars.vue
+++ components/review-stars.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="flex review-stars" :style="{width: `60px`, height: `9px`}">
-    <!-- fill in here -->
+  <div
+    class="flex flex-row review-stars"
+    :style="{ width: `60px`, height: `9px`, marginBottom: '2px' }"
+  >
+    <icon type="starFull" v-for="n in stars" :key="`Full${n}`" />
+    <icon type="starEmpty" v-for="n in numEmptyStars" :key="`Empty${n}`" />
   </div>
 </template>
 
 <script>
 import Icon from '#components/icon'
+
+const maxStarRating = 5
 
 export default {
   name: 'review-stars',
@@ -13,6 +19,10 @@ export default {
   components: {
     Icon
   },
-  computed: {}
+  computed: {
+    numEmptyStars: function() {
+      return maxStarRating - this.stars
+    }
+  }
 }
 </script>
