bc70e82dac06aad24da355721d2b547a2826e3b4
Mekodeng94 <mekodeng94@gmail.com>
2020-08-11 11:28:03 -0400

    Added product image and details

--- components/product-card.vue
+++ components/product-card.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <div class="text-left">
-      <transition name="fade">
-        <lazy-image :src="product.images.main" v-if="isHovering" />
-        <lazy-image :src="product.images.secondary" v-else />
+    <div class="text-left" @mouseover="isHovering=true" @mouseleave="isHovering=false">
+      <transition name="fade" mode="out-in">
+        <lazy-image key=1 :src="product.images.secondary" v-if="isHovering" />
+        <lazy-image key=2 :src="product.images.main" v-else />
       </transition>
-      <div class="flex">
-        <!-- fill in here -->
+      <div class="flex flex-row justify-between items-end pt-1">
+        <div class="flex-1">
+          <p class="font-display text-lg leading-xl">{{product.title}}</p>
+          <p class="font-body1 text-xs leading-base">{{product.byline}}</p>
+        </div>
+        <div class="flex flex-1 flex-col items-end justify-center">
+          <p class="font-body1 text-sm leading-lg ">{{product.price}}</p>
+          <ReviewStars :stars="product.reviews.average"/>
+          <p class="font-body1 text-xs leading-base">{{`(${product.reviews.count} Reviews)`}}</p>
+        </div>
     </div>
   </div>
 </template>
@@ -24,7 +32,7 @@ export default {
   },
   data() {
     return {
-      isHovering: false
+      isHovering: false,
     }
   }
 }
